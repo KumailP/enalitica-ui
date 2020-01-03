ded5ef0413b65baa977f3a97ad53f91f82a06bf0
Kumail Pirzada <kumailpirzada@gmail.com>
2020-01-03 12:25:38 +0500

    Navbar home style update

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.scss";
+
+export default function index() {
+  return (
+    <div className="about">
+      <h1>What is Enalitica?</h1>
+      <p>
+        Enalitica generates a detailed analysis of historical data of various
+        companies or organizations, and makes predictions based on various
+        criteria such as grouping, timeline, etc. to empower executives to make
+        the right decisions.
+      </p>
+    </div>
+  );
+}
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.scss";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
+import About from "../../components/About";
 
 export default () => {
   return (
     <div className="home">
       <Navbar />
       <Header />
+      <About />
     </div>
   );
 };
